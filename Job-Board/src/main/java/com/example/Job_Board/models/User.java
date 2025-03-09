16f0f5a99cdd0cacfd7a014fcd44c4223e53713a
dido1043 <dudunovski_05@abv.bg>
2025-03-09 19:55:24 +0200

    Added new dependencies and job_post entiy
    

---
+++ Job-Board/src/main/java/com/example/Job_Board/models/User.java
@@ -0,0 +1,31 @@
+package com.example.Job_Board.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String passwordHash;
+
+    @Column(nullable = false, length = 20)
+    private String role; // 'JOB_SEEKER' or 'RECRUITER'
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
