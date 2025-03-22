98ee822bfffb781a07729912319175fd98fe9666
dido1043 <dudunovski_05@abv.bg>
2025-03-22 18:21:01 +0200

    up

--- Job-Board/src/main/java/com/example/Job_Board/repository/ApplicationRepository.java
+++ Job-Board/src/main/java/com/example/Job_Board/repository/ApplicationRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-//    List<Application> findByApplicantId(Long id);
+    List<Application> findByJobSeekerId(Long id);
 }
