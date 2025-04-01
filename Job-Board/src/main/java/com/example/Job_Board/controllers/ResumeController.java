8b6b60f8c40713efe7658ed051d4e23fc0626822
dido1043 <dudunovski_05@abv.bg>
2025-04-01 11:14:53 +0300

    editResume_endpoint_ready->Ready

--- Job-Board/src/main/java/com/example/Job_Board/controllers/ResumeController.java
+++ Job-Board/src/main/java/com/example/Job_Board/controllers/ResumeController.java
@@ -16,18 +16,24 @@ public class ResumeController {
     public ResumeController(ResumeService resumeService) {
         this.resumeService = resumeService;
     }
+
     @GetMapping("/get/{id}")
-    public ResponseEntity<?> getById(@PathVariable  Long id) {
+    public ResponseEntity<?> getById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(resumeService.getResumeById(id));
     }
 
     @GetMapping("/all")
-    public ResponseEntity<?> allResumes(){
+    public ResponseEntity<?> allResumes() {
         return ResponseEntity.status(HttpStatus.OK).body(resumeService.getAllResumes());
     }
-    
+
     @PostMapping("/add")
     public ResponseEntity<?> addResume(@RequestBody ResumeDto resumeDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(resumeService.craeteResume(resumeDto));
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<?> editResume(@PathVariable Long id, @RequestBody ResumeDto resumeDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(resumeService.updateResume(id, resumeDto));
     }
 }
