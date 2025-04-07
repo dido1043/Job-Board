28819eeac9202763e2e59c51b480daaba182b117
dido1043 <dudunovski_05@abv.bg>
2025-04-07 22:34:13 +0300

    up

--- Job-Board/src/main/java/com/example/Job_Board/services/impl/UserServiceImpl.java
+++ Job-Board/src/main/java/com/example/Job_Board/services/impl/UserServiceImpl.java
@@ -3,16 +3,17 @@ package com.example.Job_Board.services.impl;
 import com.example.Job_Board.models.entity.User;
 import com.example.Job_Board.repository.UserRepository;
 import com.example.Job_Board.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-
+    @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
 
     @Override
     public List<String> addSkills(Long userId, List<String> skills) {
@@ -31,6 +32,6 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         user.setSeniority(seniority);
-        return "Ready";
+        return user.getSeniority();
     }
 }
