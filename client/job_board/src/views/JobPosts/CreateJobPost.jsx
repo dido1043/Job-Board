5ae3104cee32cd2646473057fe27623b7cfcdf0c
dido1043 <dudunovski_05@abv.bg>
2025-04-10 00:01:20 +0300

    up

---
+++ client/job_board/src/views/JobPosts/CreateJobPost.jsx
@@ -0,0 +1,14 @@
+const CreateJobPost = () => {   
+    // Logic for creating a job post goes here
+    // This might include form handling, API calls, etc.    
+    // For example, you might want to use useState to manage form inputs
+    // and useEffect to fetch any necessary data when the component mounts.
+    // Example of using useState for form inputs
+        
+    return (
+        <div>
+        <h1>Create Job Post</h1>
+        {/* Add form for creating a job post */}
+        </div>
+    );
+}
