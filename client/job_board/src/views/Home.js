a31dbb545f53f476e507f96ec9bcb600c14e8867
dido1043 <dudunovski_05@abv.bg>
2025-04-09 16:22:28 +0300

    up

---
+++ client/job_board/src/views/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Job Board</h1>
+            <p>Find your dream job here!</p>
+        </div>
+    );
+}
+export default Home;
