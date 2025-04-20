e38cc309e31e08032754112866c7039af3b43acd
dido1043 <dudunovski_05@abv.bg>
2025-04-20 20:29:19 +0300

    up

--- client/job_board/src/views/User/UserPage.jsx
+++ client/job_board/src/views/User/UserPage.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputField from '../../components/shared/InputField';
 import BaseButton from '../../components/shared/BaseButton';
 import axios from 'axios';
 const UserPage = () => {
   //todo: add user page logic
-  const [seniority, setSeniority] = useState(null);
+  const [seniority, setSeniority] = useState();
   const [skills, setSkills] = useState([]);
+  const [allSkills, setAllSkills] = useState([]);
   const [userId, setUserId] = useState(localStorage.getItem('userId'));
 
   const [error, setError] = useState({
     message: ''
   });
 
-  const handleSkillsChange = (e) => { 
-    
+  const handleSkillsChange = (e) => {
+
     setSkills(e.target.value.split(',').map(skill => skill.trim()));
   }
   const addSkillsToUser = async (e) => {
@@ -25,46 +26,91 @@ const UserPage = () => {
           'Accept': '*/*'
         }
       });
-      console.log(response.data);
+      setAllSkills(response.data);
+      setSkills([]);
+      console.log(allSkills);
+      //console.log(response.data);
     } catch (error) {
       setError({
         message: 'Error adding skills to user'
       });
     }
   }
-  //--take user id from local storage  
-  //--fetch user data from backend
+
+
+  useEffect(() => {
+    const fetchUserSkills = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/user/show-skills/${userId}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': '*/*'
+          }
+        });
+        setAllSkills(response.data);
+        console.log(allSkills);
+      } catch (error) {
+        setError({
+          message: 'Error fetching user skills'
+        });
+      }
+    }
+    fetchUserSkills()
+  }, [userId])
+
+  const seniorityOnChange = (e) => {
+    setSeniority(e.target.value);
+  };
+  const addSeniorityToUser = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:8080/user/set-seniority/${userId}`, {seniority} , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': '*/*'
+        }
+      });
+      console.log(response.data);
+      if (response.data !== seniority) {
+        setSeniority(response.data);
+      }
+    } catch (error) {
+      setError({
+        message: 'Error adding seniority to user'
+      });
+    }
+
+  }
   return (
     <div className="container mt-5">
       <form>
         <div className="mb-3">
-          <InputField label="Add Skill:" type="text" name="skills" id="skills"  onChange={handleSkillsChange} className="form-control" placeholder="Enter skill" />
+          <InputField label="Add Skill:" type="text" name="skills" id="skills" onChange={handleSkillsChange} className="form-control" placeholder="Enter skill" />
 
-          {skills.length > 0 ?
+          {allSkills.length > 0 ?
             <div className="mt-2">
-              <p>Skills: {skills.join(', ')}</p>
+              <p>Skills: {allSkills.map(s => <li>{s}</li>)}</p>
             </div> : <></>
           }
-          <BaseButton text="Add skill" type="submit" onClick={addSkillsToUser}/>
+          <BaseButton text="Add skill" type="submit" onClick={addSkillsToUser} />
         </div>
         <div className="mb-3">
           <label htmlFor="seniority" className="form-label">Set Seniority:</label>
           <select
             id="seniority"
             className="form-select"
-            onChange={(e) => setSeniority(e.target.value)}
+            onChange={seniorityOnChange}
+            value={seniority}
           >
+            <option value="" disabled>Select seniority</option>
+            <option value="Intern">Intern</option>
             <option value="Junior">Junior</option>
             <option value="Mid">Mid</option>
             <option value="Senior">Senior</option>
           </select>
           <div className="d-flex gap-2 mt-2">
 
-            <BaseButton text="Set Seniority" type="button" onClick={() => {
-                // Logic to set seniority
-                
-                console.log(`Seniority set to: ${seniority}`);
-              }}/>
+            <BaseButton text="Set Seniority" type="button" onClick={addSeniorityToUser} />
 
           </div>
         </div>
