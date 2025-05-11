5c61f5b351204de11373ca7b4d03b3f659e0189b
dido1043 <dudunovski_05@abv.bg>
2025-05-11 18:01:39 +0300

    Update ResumeController CORS configuration and refine database URL in application properties

--- Job-Board/src/main/java/com/example/Job_Board/controllers/ResumeController.java
+++ Job-Board/src/main/java/com/example/Job_Board/controllers/ResumeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/resume")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ResumeController {
     private final ResumeService resumeService;
 
