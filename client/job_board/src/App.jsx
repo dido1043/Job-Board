ae9911933b5e1eccf7c4601c71fec3335a4cba98
dido1043 <dudunovski_05@abv.bg>
2025-05-19 15:05:34 +0300

    Remove unused useLocation import from App component

--- client/job_board/src/App.jsx
+++ client/job_board/src/App.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './views/Home';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 import AllJobPosts from './views/JobPosts/AllJobPosts';
